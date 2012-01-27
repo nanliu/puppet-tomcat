dd5a9bd5ae5763d7832aeb1ba72df1e1952fea23
Nan Liu <nan@puppetlabs.com>
2012-01-27 12:10:32 -0500

    Add service manifests and template.

---
+++ manifests/service.pp
@@ -0,0 +1,35 @@
+# Class: tomcat::service
+#
+#   tomcat service
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class tomcat::service (
+  $service_name = hiera('tomcat_service_name', 'tomcat'),
+  $template     = hiera('tomcat_template', "tomcat/tomcat.${::osfamiy}.erb"),
+  $target       = hiera('tomcat_target', '/opt')
+){
+
+  $tomcat_home = "${target}/tomcat"
+
+  file { '/etc/init.d/tomcat':
+    owner => 'root',
+    group => 'root',
+    mode  => '0755',
+    content  => template($template),
+  }
+
+  service { $service_name:
+    ensure     => running,
+    hasrestart => true,
+    hasstatus  => true,
+    require    => File['/etc/init.d/tomcat'],
+    #subscribe  => Class['tomcat::config'],
+  }
+}
