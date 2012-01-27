669f99ef950a82afa2d907f85a82ec0c7900ab6d
Nan Liu <nan@puppetlabs.com>
2012-01-27 10:20:01 -0500

    Initial commit.

---
+++ manifests/package.pp
@@ -0,0 +1,87 @@
+# Class: tomcat::package
+#
+#   Installs tomcat software either via binary or packages.
+#
+# Parameters:
+#   * uid: tomcat user id.
+#   * gid: tomcat user gid.
+#   * shell: tomcat user shell.
+#   * deployment: tomcat deployment method, either file or package.
+#   * package_name: tomcat software package name.
+#   * version: tomcat software version (for package deploymetn also support installed, latest).
+#   * source: tomcat software source for file install. (supports puppet:// as well as http://)
+#   * target: tomcat software installation directory
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class tomcat::package (
+  $uid          = hiera('tomcat_uid', 201),
+  $gid          = hiera('tomcat_gid', 201),
+  $shell        = hiera('tomcat_shell', '/bin/nologin'),
+  $deployment   = hiera('tomcat_deployment', 'file'),
+  $package_name = hiera('tomcat_package_name', 'tomcat'),
+  $version      = hiera('tomcat_version', 'apache-tomcat-6.0.35'),
+  # settings below only relevant for file deployment
+  $source       = hiera('tomcat_source', 'http://apache.mirrorcatalogs.com/tomcat/tomcat-6/v6.0.35/bin/apache-tomcat-6.0.35.tar.gz'),
+  $target       = hiera('tomcat_target', '/opt')
+) {
+
+  include staging
+
+  group { 'tomcat':
+    ensure => present,
+    gid    => $uid,
+  }
+
+  user { 'tomcat':
+    ensure => present,
+    uid    => $uid,
+    gid    => $gid,
+    shell  => $shell,
+  }
+
+  case $deployment {
+    'file': {
+      staging::file { $version:
+        source => $source,
+      }
+
+      staging::extract { $version:
+        target => $target,
+        require => Staging::File[$version],
+      }
+
+      $tomcat_dir = "${target}/${version}"
+
+      file { $tomcat_dir:
+        ensure   => directory,
+        recurse  => true,
+        owner    => 'tomcat',
+        group    => 'tomcat',
+        checksum => 'none',
+        require  => Staging::Extract[$version],
+      }
+
+      file { "${target}/tomcat":
+        ensure => symlink,
+        target => $tomcat_dir,
+        require => File[$tomcat_dir],
+      }
+    }
+
+    'package': {
+      package { $package_name:
+        ensure => $version,
+      }
+    }
+
+    default: {
+      fail("tomcat::package: unknown deployment method ${deployment}")
+    }
+  }
+
+}
---
+++ tests/package.pp
@@ -0,0 +1,1 @@
+class { 'tomcat::package': }
